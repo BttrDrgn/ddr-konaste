d1f0353e661eb81bb0d75b558acb2b5fb67cee59
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2021-09-01 16:11:45 -0500

    Return false on debugger present

--- src/client/game/core/core.cpp
+++ src/client/game/core/core.cpp
@@ -9,6 +9,18 @@ namespace ddr::game
 	float core::frame_rate;
 	bool core::judegment;
 
+	utils::hook::detour debugger_present_hook;
+	bool debugger_present()
+	{
+		return false;
+	}
+
+	utils::hook::detour exit_process_hook;
+	void exit_process(::UINT)
+	{
+		return;
+	}
+
 	void core::init()
 	{
 		core::frame_rate = 0.0f;
@@ -20,6 +32,9 @@ namespace ddr::game
 
 		//Disable IsDebuggerPresent from exiting process
 		utils::hook::nop(0x000000014001D81C, 5);
+
+		debugger_present_hook.create(&::IsDebuggerPresent, &debugger_present);
+		//exit_process_hook.create(&::ExitProcess, &exit_process);
 
 		callbacks::on(callbacks::type::settings_saved, []
 		{
